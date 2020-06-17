311fd631ba37dec9f3f4f8e09f4dbea47af720a2
alekkryloff <aleck.101@yandex.ru>
2020-06-17 22:19:47 +0300

    Updates

---
+++ precache-manifest.03fc00467daafcf92fc3904d7286e180.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c23c165e58fc2934e1cfe6c75c41b82c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "216f26f439165d9de255",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e85459bd4233d81d8803",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "216f26f439165d9de255",
+    "url": "./static/js/2.3cf993a6.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.3cf993a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e85459bd4233d81d8803",
+    "url": "./static/js/main.f713343b.chunk.js"
+  },
+  {
+    "revision": "556dfe5fcd8d7c77ee4f",
+    "url": "./static/js/runtime-main.c21f4b73.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
